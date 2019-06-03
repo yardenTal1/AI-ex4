ff945ae99e1f3fd5a617d6fc27b684dc9ac181d7
netta.barak <netta.barak@cs.huji.ac.il>
2019-06-03 21:10:13 +0300

    redid Q3

--- analysis.py
+++ analysis.py
@@ -12,46 +12,42 @@
 
 # Change these default values to obtain the specified policies through
 # value iteration.
-
+# TODO compare answers
 def question2():
   answerDiscount = 0.9
   answerNoise = 0
   return answerDiscount, answerNoise
 
 def question3a():
-  answerDiscount = 0.3
-  answerNoise = 0.5
+  answerDiscount = 0.1
+  answerNoise = 0.0
   answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
-  return answerDiscount, answerNoise, answerLivingReward
+  return 'NOT POSSIBLE'
   # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-  answerDiscount = 0.9
-  answerNoise = 0.2
+  answerDiscount = 0.5
+  answerNoise = 0.0
   answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
   answerDiscount = 0.9
   answerNoise = 0.2
   answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
+
 
 def question3e():
   answerDiscount = 0.9
   answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerLivingReward = 100.0
   return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
+
 
 def question6():
   answerEpsilon = None
